6f9312a08ff5eccb3758a60e126d8dc2a071dbcf
Bradley <breg2851@uni.sydney.edu.au>
2019-10-04 15:48:43 +1000

    add Xcode file

---
+++ TransportAccessibility/TransportAccessibility/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TransportAccessibility
+//
+//  Created by Bradley Rego on 4/10/19.
+//  Copyright © 2019 Bradley Rego. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
